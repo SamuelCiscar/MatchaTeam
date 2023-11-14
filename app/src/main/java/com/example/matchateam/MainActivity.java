3bb674b228de5eab6fede234ab73911307ab0f5c
Coralie Fonta <fonta.coralie@hotmail.fr>
2023-11-14 16:25:06 +0100

    Link Panier

--- app/src/main/java/com/example/matchateam/MainActivity.java
+++ app/src/main/java/com/example/matchateam/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.matchateam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.matchateam.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btCart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == binding.btCart){
+            Intent intent = new Intent(this, PanierActivity.class);
+            startActivity(intent);
+        }
     }
 }
