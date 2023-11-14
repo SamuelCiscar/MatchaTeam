ac6ec435643581e3b9eadec53852e207b2215b6b
SamuelCiscar <133649133+SamuelCiscar@users.noreply.github.com>
2023-11-14 10:20:08 +0100

    c'est un peu moins le chaos

---
+++ app/src/main/java/com/example/matchateam/RequestUtils.java
@@ -0,0 +1,40 @@
+package com.example.matchateam;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RequestUtils {
+    private static final String MON_API = "urlApi";
+    private final Gson gson;
+    private final OkHttpClient client;
+
+    public RequestUtils() {
+        this.gson = new Gson();
+        this.client = new OkHttpClient();
+    }
+
+    public String sendPost(String url, String jsontoSend) {
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), jsontoSend);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body() != null ? response.body().string() : null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
