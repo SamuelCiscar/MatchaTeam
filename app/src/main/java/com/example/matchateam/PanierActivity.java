0e73a942d0fb2b654d619c647c324209d240a5fc
SamuelCiscar <133649133+SamuelCiscar@users.noreply.github.com>
2023-11-15 09:43:21 +0100

    Visuel terminé

--- app/src/main/java/com/example/matchateam/PanierActivity.java
+++ app/src/main/java/com/example/matchateam/PanierActivity.java
@@ -1,17 +1,32 @@
 package com.example.matchateam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PanierActivity extends AppCompatActivity {
+import com.example.matchateam.databinding.ActivityPanierBinding;
+
+public class PanierActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ActivityPanierBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panier);
+        binding = ActivityPanierBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.logohome.setOnClickListener(this);
     }
 
 
-
-
+    @Override
+    public void onClick(View v) {
+        if(v == binding.logohome){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+    }
+}
 }
