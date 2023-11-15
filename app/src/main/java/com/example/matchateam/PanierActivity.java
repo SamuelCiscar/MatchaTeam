8bf36f6de12928d5fee168f02221d87c55259084
SamuelCiscar <133649133+SamuelCiscar@users.noreply.github.com>
2023-11-15 10:25:09 +0100

    bouton valider v1

--- app/src/main/java/com/example/matchateam/PanierActivity.java
+++ app/src/main/java/com/example/matchateam/PanierActivity.java
@@ -3,14 +3,20 @@ package com.example.matchateam;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.matchateam.databinding.ActivityPanierBinding;
 
-public class PanierActivity extends AppCompatActivity implements View.OnClickListener{
+public class PanierActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityPanierBinding binding;
+    private EditText editTextPhone;
+    private EditText etprenom;
+    private EditText etName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +24,40 @@ public class PanierActivity extends AppCompatActivity implements View.OnClickLis
         binding = ActivityPanierBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.logohome.setOnClickListener(this);
-    }
 
+        editTextPhone = binding.editTextPhone;
+        etprenom = binding.etprenom;
+        etName = binding.etName;
+
+        binding.logohome.setOnClickListener(this);
+        binding.btnValidate.setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View v) {
-        if(v == binding.logohome){
+        if (v == binding.logohome) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        }
+
+        if (v == binding.btnValidate) {
+            String nom = etName.getText().toString().trim();
+            String prenom = etprenom.getText().toString().trim();
+            String telephone = editTextPhone.getText().toString().trim();
+
+            if (!nom.isEmpty() && !prenom.isEmpty() && !telephone.isEmpty()) {
+                // Si tous les champs sont remplis, afficher le toast de validation
+                Toast.makeText(this, "Votre commande est validée", Toast.LENGTH_SHORT).show();
+                // Vider le contenu des EditText
+                etName.setText("");
+                etprenom.setText("");
+                editTextPhone.setText("");
+            } else {
+                // Si l'un des champs est vide, afficher un message demandant de remplir tous les champs
+                Toast.makeText(this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
 }
-}
+
